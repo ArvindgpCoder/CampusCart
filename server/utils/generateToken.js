3c4ede0aa7d081f611e59adf116c47d94231689e
Arvind <Arvindgpta786@gmail.com>
2024-09-15 15:50:43 +0530

    some changes

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const generateTokens = async (user) => {
   try {
     const payload = { _id: user._id };
-    const token = jwt.sign(payload, process.env.KEY, {
+    const token = jwt.sign(payload, "wejhbqjkfb", {
       expiresIn: "1d",
     });
     console.log(token);
--- server/utils/verifyToken.js
+++ server/utils/verifyToken.js
@@ -1,16 +1,16 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = async (Token) => {
-  const privateKey = process.env.KEY;
+  const privateKey = "wejhbqjkfb";
   return new Promise((resolve, reject) => {
     jwt.verify(Token, privateKey, (err, tokenDetails) => {
       if (err) {
-        return reject({ error: false, message: "Invalid refresh token" });
+        return reject({ error: false, message: "Invalid token" });
       }
       resolve({
         tokenDetails,
         error: false,
-        message: "Valid refresh token",
+        message: "Valid token",
       });
     });
   });
